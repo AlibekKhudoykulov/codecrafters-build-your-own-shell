fb481ca8d29ec1890e2eea65273e8f6edbfda421
AlibekKhudoykulov <AlibekKhudoykulov@users.noreply.github.com>
2026-05-14 16:17:25 +0500

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -69,7 +69,7 @@ public class Main {
 
     private static void handlePath(String paths) {
         if(paths.startsWith("~")){
-            currentDirectory = System.getProperty("user.dir");;
+            currentDirectory = System.getProperty("user.dir");
             return;
         }
 
